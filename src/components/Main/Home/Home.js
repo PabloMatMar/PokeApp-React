812a2bc2ef027de7fef3d9130618306a4d561da3
PabMatMar <pabmatmar@gmail.com>
2023-03-18 21:37:52 +0100

    add/ Set search limit to 151

--- src/components/Main/Home/Home.js
+++ src/components/Main/Home/Home.js
@@ -15,7 +15,7 @@ const Home = () => {
     async function fetchData() {
       if (dataPokemons.length <= 1) {
         try {
-          const res = await axios.get(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=902`);
+          const res = await axios.get(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=151`); //Cambiar 151 por 902 para obtenerlos todos
           const pokemons = res.data.results
 
           const urls = pokemons.map(pokemon => pokemon.url)
