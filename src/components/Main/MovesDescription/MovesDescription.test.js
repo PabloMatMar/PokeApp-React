e83b0c4b4e77403fba0839657aea39ef286d287b
PabMatMar <pabmatmar@gmail.com>
2023-04-11 17:29:41 +0200

    add/ Basic test to component MovesDescription is ok

--- src/components/Main/MovesDescription/MovesDescription.test.js
+++ src/components/Main/MovesDescription/MovesDescription.test.js
@@ -1,12 +1,32 @@
 import React from "react";
 import { render, screen } from '@testing-library/react';
 import MovesDescription from "./MovesDescription";
+import { pokemonContext } from "../../../context/pokemonContext";
 
 describe("MovesDescription", () => {
+
   test("matches snapshot", () => {
+    const simulatedDataOfContext = {
+      data: {
+        accuracy: 80,
+        damage_class: {
+          name: "Pyshical"
+        },
+        effect_entries: [
+          {
+            effect: "Im a testing Move!",
+            short_effect: "Me to"
+          }
+        ]
+      },
+    }
     render(
-    <MovesDescription />
+      <pokemonContext.Provider value={simulatedDataOfContext}>
+        <MovesDescription />
+      </pokemonContext.Provider>
     );
     expect(screen).toMatchSnapshot();
   });
+
 });
+
