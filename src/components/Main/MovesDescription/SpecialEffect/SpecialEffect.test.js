2819f8f748ed8510fa865e315bcfe7513551acc1
PabMatMar <pabmatmar@gmail.com>
2023-04-11 20:17:10 +0200

    add/ test of component SpecialEffect is ok

--- src/components/Main/MovesDescription/SpecialEffect/SpecialEffect.test.js
+++ src/components/Main/MovesDescription/SpecialEffect/SpecialEffect.test.js
@@ -1,10 +1,41 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { render, screen } from '@testing-library/react';
 import SpecialEffect from "./SpecialEffect";
+import { pokemonContext } from "../../../../context/pokemonContext";
 
 describe("SpecialEffect", () => {
   test("matches snapshot", () => {
-    const wrapper = shallow(<SpecialEffect />);
-    expect(wrapper).toMatchSnapshot();
+
+    const simulatedProps = {
+      data: {
+        url: 'https://pokeapi.co/api/v2/contest-effect/32/'
+      }
+    }
+
+    const simulatedDataOfContext = {
+      specialEffect: {
+
+        effect_entries: [
+          {
+            effect: "User gains one star."
+          }
+
+        ],
+        flavor_text_entries: [
+          {
+            flavor_text: "Ups the user's condition.  Helps prevent nervousness."
+          }
+        ]
+      }
+    }
+
+
+    render(
+      <pokemonContext.Provider value={simulatedDataOfContext}>
+        <SpecialEffect data={simulatedProps} />
+
+      </pokemonContext.Provider>
+    );
+    expect(screen).toMatchSnapshot();
   });
 });
