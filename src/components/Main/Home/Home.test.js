51b1be680fbd4fa3f77bb79f6dc3a59f9e1f80b5
PabMatMar <pabmatmar@gmail.com>
2023-04-17 13:12:17 +0200

    In the previous commit, unit tests were also adapted to changes

--- src/components/Main/Home/Home.test.js
+++ src/components/Main/Home/Home.test.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { render, screen } from '@testing-library/react';
 import Home from "./Home";
 import { newPokemonContext } from '../../../context/newPokemonContext';
